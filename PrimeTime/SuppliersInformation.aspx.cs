52af3b709881eb0b3a06e724f0c94cc6b46ba1db
cgooch1 <cgooch1@my.apsu.edu>
2014-04-29 16:04:17 -0500

    working on adding sql calls to code

--- PrimeTime/SuppliersInformation.aspx.cs
+++ PrimeTime/SuppliersInformation.aspx.cs
@@ -4,16 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 public partial class SuppliersInformation : System.Web.UI.Page
 {
+    SqlConnection conn = new SqlConnection("Data Source=apcxcs3.apsu.edu;Initial Catalog=group3_6;Persist Security Info=True;User ID=webuser3_6;Password=webuser3_6abc");
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
     protected void submit_btn_Click(object sender, EventArgs e)
     {
-
+        if (chooseAction_ddl.SelectedIndex == 1)
+        {
+            SqlCommand command = new SqlCommand("new_suppliers", conn);
+        }
     }
     protected void clear_btn_Click(object sender, EventArgs e)
     {
