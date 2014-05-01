7bf544b088a8dd9cb9bcab400089dc50b31d38a0
CJ Gooch <cgooch1@my.apsu.edu>
2014-05-01 18:40:46 -0500

    updated Reports screen

--- PrimeTime/ReportsGeneration.aspx.cs
+++ PrimeTime/ReportsGeneration.aspx.cs
@@ -21,13 +21,7 @@ public partial class ReportsGeneration : System.Web.UI.Page
     }
     protected void generateReport_btn_Click(object sender, EventArgs e)
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-=======
->>>>>>> f5108468279559ad49f05d5805767effaf6fd703
         if (chooseReport_ddl.SelectedIndex == 0)
         {
             Response.Redirect("AccountsPayableReports.aspx");
@@ -70,12 +64,6 @@ public partial class ReportsGeneration : System.Web.UI.Page
     protected void chooseReport_ddl_SelectedIndexChanged(object sender, EventArgs e)
     {
 
-<<<<<<< HEAD
-=======
-        
->>>>>>> 905ac388bd0890f28645859866f716432f64ee6e
->>>>>>> 5b7a82591ead5522b50b0342f358bed6c628a3ff
-=======
->>>>>>> f5108468279559ad49f05d5805767effaf6fd703
+
     }
 }
