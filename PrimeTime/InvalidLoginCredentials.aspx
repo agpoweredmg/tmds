d193846173583ba371f9e8640a0a0d86a3712e38
cgooch1 <cgooch1@my.apsu.edu>
2014-05-01 00:05:12 -0500

    made the check_for_mgr sp return a useful fucking value in c# bitches

---
+++ PrimeTime/InvalidLoginCredentials.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="InvalidLoginCredentials.aspx.cs" Inherits="InvalidLoginCredentials" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .style1
+        {
+            width: 666px;
+            height: 400px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <center><img alt="" class="style1" src="youHaveNoPowerHere.jpg" /></center>
+    </div>
+    <div>
+       <center> <asp:Button ID="Button1" runat="server" Text="Return to Login Page" 
+               Height="50px" onclick="Button1_Click" /> </center>
+    </div>
+    </form>
+</body>
+</html>
--- PrimeTime/Login.aspx.cs
+++ PrimeTime/Login.aspx.cs
@@ -4,15 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 public partial class Login : System.Web.UI.Page
 {
+    private SqlConnection conn = new SqlConnection("Data Source=apcxcs3.apsu.edu;Initial Catalog=group3_6;Persist Security Info=True;User ID=webuser3_6;Password=webuser3_6abc");
+    private SqlCommand command;
+    private HttpCookie credential = new HttpCookie("lcCookie");
+    private DateTime loginTime = DateTime.Now;
+    private SqlDataReader reader;
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
     protected void login_btn_Click(object sender, EventArgs e)
     {
+        command = new SqlCommand("check_for_mgr", conn);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.AddWithValue("@empid", userName_txt.Text.ToString());
+        command.Parameters.AddWithValue("@emppass", passWord_txt.Text.ToString());
+        command.Parameters.Add("@mgr", SqlDbType.Bit).Direction = ParameterDirection.Output;
 
+        conn.Open();
+        command.ExecuteNonQuery();
+        bool mgr = Convert.ToBoolean(command.Parameters["@mgr"].Value);
+        conn.Close();
+
+        Label1.Text = mgr.ToString();
     }
 }
