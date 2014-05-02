80d98829aad2b1af20da79cc81085b4b6a7631e8
cgooch1 <cgooch1@my.apsu.edu>
2014-05-01 20:28:37 -0500

    updating

--- Documentation/Insert_Queries/Category.sql
+++ Documentation/Insert_Queries/Category.sql
@@ -1,6 +1,6 @@
 INSERT into Category(Category_ID, Cateory)
-values('00000001', 'Meat'),
-('00000002', 'Bread'),
-('00000003', 'Vegtables'),
-('00000004', 'Dairy'),
-('00000005', 'Snacks')
+values	('Meat'),
+		('Poultry'),
+		('Fresh Produce'),
+		('Prepared Salads'),
+		('Snacks')
