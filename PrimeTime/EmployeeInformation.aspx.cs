7e4c38a4513552e47d41bf65747f67515e0994dc
CJ Gooch <cgooch1@my.apsu.edu>
2014-05-02 12:17:37 -0500

    updated employees

--- PrimeTime/EmployeeInformation.aspx.cs
+++ PrimeTime/EmployeeInformation.aspx.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
 
 public partial class EmployeeInformation : System.Web.UI.Page
 {
+    private SqlConnection conn = new SqlConnection("Data Source=apcxcs3.apsu.edu;Initial Catalog=group3_6;Persist Security Info=True;User ID=webuser3_6;Password=webuser3_6abc");
+    private SqlCommand command;
+
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!Convert.ToBoolean(Request.Cookies["cred"]["mgr"]))
@@ -16,7 +22,63 @@ public partial class EmployeeInformation : System.Web.UI.Page
     }
     protected void submit_btn_Click(object sender, EventArgs e)
     {
+        if (chooseAction_ddl.SelectedIndex == 1)
+        {
+            SqlCommand command = new SqlCommand("new_employee", conn);
 
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@emp_ssn", ssn_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_f_name", FirstName_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_l_name", lastName_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_phone", phone_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_alt_phone", altPhone_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_hir_date", hireDate_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_end_date", endDate_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_pass", password_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_manager_y_n", manager_chkbx.Text.ToString());
+
+            conn.Open();
+
+            command.ExecuteNonQuery();
+
+            conn.Close();
+        }
+        if (chooseAction_ddl.SelectedIndex == 3)
+        {
+            SqlCommand command = new SqlCommand("update_employee", conn);
+
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@emp_id", ssn_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_ssn", ssn_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_f_name", FirstName_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_l_name", lastName_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_phone", phone_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_alt_phone", altPhone_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_hir_date", hireDate_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_end_date", endDate_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_pass", password_tb.Text.ToString());
+            command.Parameters.AddWithValue("@emp_manager_y_n", manager_chkbx.Text.ToString());
+
+            conn.Open();
+
+            command.ExecuteNonQuery();
+
+            conn.Close();
+        }
+        if (chooseAction_ddl.SelectedIndex == 2)
+        {
+            SqlCommand command = new SqlCommand("delete_employee", conn);
+
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@emp_id", id_tb.Text.ToString());
+            conn.Open();
+
+            command.ExecuteNonQuery();
+
+            conn.Close();
+        }
     }
     protected void clear_btn_Click(object sender, EventArgs e)
     {
